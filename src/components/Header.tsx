e17e513763d0f9b23a498f0287c105dd17bca06d
Colin Huber <cbhuber@gmail.com>
2026-04-25 11:03:04 -0600

    env: Initial project files

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="title">🍡 Kirby Mochi Recepy 🍡</h1>
+      <p className="subtitle">Cute, soft, and delicious!</p>
+    </header>
+  );
+}
+
+export default Header;
